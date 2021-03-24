4a3a93b6cae8123c347080aa863a415a4b054d38
suding <betty.sujin.lee@gmail.com>
2021-03-24 11:06:38 +0900

    Customizing Navigation Controller..

--- CustomizeNavigationBar/ViewController.swift
+++ CustomizeNavigationBar/ViewController.swift
@@ -11,9 +11,52 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.initTitle()
+        self.initTitleNew()
+        //self.initTitleImage()
     }
 
+    func initTitle(){
+        let nTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+        nTitle.numberOfLines = 2
+        nTitle.textAlignment = .center
+        nTitle.font = UIFont.systemFont(ofSize: 15)
+        nTitle.text = "58개 숙소 \n 1박(1월 10일 ~ 1월 11일)"
+        
+        self.navigationItem.titleView = nTitle
+        
+        let color = UIColor(red: 0.02, green: 0.22, blue: 0.49, alpha: 1.0)
+        self.navigationController?.navigationBar.barTintColor = color
+    }
 
+    func initTitleNew(){
+        let containerView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 36))
+        let topTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 18))
+        topTitle.numberOfLines = 1
+        topTitle.textAlignment = .center
+        topTitle.font = UIFont.systemFont(ofSize: 15)
+        topTitle.textColor = .white
+        topTitle.text = "58개 숙소"
+        
+        let subTitle = UILabel(frame: CGRect(x: 0, y: 18, width: 200, height: 18))
+        subTitle.numberOfLines = 1
+        subTitle.textAlignment = .center
+        subTitle.font = UIFont.systemFont(ofSize: 12)
+        subTitle.textColor = .white
+        subTitle.text = "1박 (1월 10일 ~ 1월 11일)"
+        
+        containerView.addSubview(topTitle)
+        containerView.addSubview(subTitle)
+        self.navigationItem.titleView = containerView
+        
+        let color = UIColor(red: 0.02, green: 0.22, blue: 0.49, alpha: 1.0)
+        self.navigationController?.navigationBar.barTintColor = color
+    }
+    
+    func initTitleImage(){
+        let image = UIImage(named: "swift_logo")
+        let imageV = UIImageView(image: image)
+        self.navigationItem.titleView = imageV
+    }
 }
 
